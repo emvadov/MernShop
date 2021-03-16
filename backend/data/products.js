e88b66d9134617cfefc9909e07bb4429a16d9f52
Emil Avadov <emvadov@gmail.com>
2021-03-16 17:24:12 +0200

    Creating server and adding concurrently

---
+++ backend/data/products.js
@@ -0,0 +1,90 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Airpods Wireless Bluetooth Headphones',
+        image: '/images/airpods.jpg',
+        description:
+            'More magical than ever. The new AirPods deliver the wireless headphone experience, reimagined.',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '159.99',
+        countInStock: 9,
+        rating: 4.8,
+        numReviews: 3,
+
+    },
+    {
+        _id: '2',
+        name: 'iPhone 12 Pro Max 128GB',
+        image: '/images/iphone12.jpg',
+        description:
+            'A14 Bionic rockets past every other smartphone chip. The Pro camera system takes low-light photography to the next level',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '998.99',
+        countInStock: 6,
+        rating: 4.5,
+        numReviews: 4,
+
+    },
+    {
+        _id: '3',
+        name: 'Sony Playstation 5 Digital Edition',
+        image: '/images/ps5.jpg',
+        description:
+            'Experience lightning-fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback',
+        brand: 'Sony',
+        category: 'Electronics',
+        price: '1149.99',
+        countInStock: 4,
+        rating: 5.0,
+        numReviews: 2,
+
+    },
+    {
+        _id: '4',
+        name: 'Sony DSLR-A200K 10.2MP Digital Camera',
+        image: '/images/dslr-a200k.jpg',
+        description:
+            'amera Kit with Super SteadyShot Image Stabilization.',
+        brand: 'Sony',
+        category: 'Electronics',
+        price: '849.99',
+        countInStock: 3,
+        rating: 4.0,
+        numReviews: 5,
+
+    },
+    {
+        _id: '5',
+        name: 'Samsung Galaxy S20 128GB',
+        image: '/images/galaxys20.jpg',
+        description:
+            'Ultra 5G With revolutionary 8K Video Snap and 5G connectivity',
+        brand: 'Samsung',
+        category: 'Electronics',
+        price: '729.99',
+        countInStock: 5,
+        rating: 4.5,
+        numReviews: 7,
+
+    },
+    {
+        _id: '6',
+        name: 'Amazon Fire TV Cube Smart Assistant Alexa',
+        image: '/images/firetv.jpg',
+        description:
+            'Hands-free streaming device with Alexa, 4K Ultra HD, 2019 release',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '119.99',
+        countInStock: 5,
+        rating: 3.5,
+        numReviews: 1,
+
+    }
+
+
+];
+
+module.exports = products;

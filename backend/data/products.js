636346a15575c9a20911a75ef4c5b54e48bea5d9
Emil Avadov <emvadov@gmail.com>
2021-03-16 17:59:21 +0200

    Adding enviroment variables and modules in node.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -87,4 +87,4 @@ const products = [
 
 ];
 
-module.exports = products;
+export default products;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const { request } = require('http')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -20,4 +22,6 @@ app.get('/api/products/:id', (req, res) => {
 
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))

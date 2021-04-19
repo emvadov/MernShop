09b1b0a70bea7ed3c07a7c5f71f659b7196b7f52
Emil Avadov <emvadov@gmail.com>
2021-04-19 16:47:59 +0300

    creating database seeder

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -70,6 +70,6 @@ const orderSchema = mongoose.Schema({
     timestamps: true
 })
 
-const Order = mongoose.model('Order', OrderSchema)
+const Order = mongoose.model('Order', orderSchema)
 
 export default Order
